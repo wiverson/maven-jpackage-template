310c4d0ee2cd94d43f64bea947e1e6f278353362
Will Iverson <wiverson@gmail.com>
2020-10-24 19:35:51 -0700

    1st commit

---
+++ src/main/java/com/doublerobot/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.doublerobot;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
