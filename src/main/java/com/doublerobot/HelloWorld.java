47410e48c32ba1f96dbf5096da530e1ef84248a5
Will Iverson <wiverson@gmail.com>
2021-03-11 14:05:17 -0800

    Refactor to no longer try to modularize the user application and Maven dependencies.

--- src/main/java/com/doublerobot/HelloWorld.java
+++ src/main/java/com/doublerobot/HelloWorld.java
@@ -10,11 +10,24 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.*;
+
 public class HelloWorld extends Application {
 
     private Button button;
 
     public static void main(String[] args) {
+
+        try {
+            File outputFile = File.createTempFile("debug", ".log", FileSystemView.getFileSystemView().getDefaultDirectory());
+            PrintStream output = new PrintStream(new BufferedOutputStream(new FileOutputStream(outputFile)), true);
+            System.setOut(output);
+            System.setErr(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         launch(args);
     }
 
